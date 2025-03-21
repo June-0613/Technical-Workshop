9551ee573a3617483e323a62dca21c50c6ee440d
June-0613 <juneelora6@gmail.com>
2025-03-21 12:47:08 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,34 +1,12 @@
 import streamlit as st
 
-st.title('Hello World')
-st.write('This is a simple Streamlit app.')
+name = st.text_input("Enter your name", "Dharani")
+password = st.text_input("Enter your password", type="password")
 
-import streamlit as st
 
-st.button("Reset", type="primary")
-if st.button("Say hello"):
-    st.write("Why hello there")
-else:
-    st.write("Goodbye")
+if st.button("Submit"):
+   if len(password) < 8:
+       st.warning("Password must be at least 8 characters long")
 
-import streamlit as st
-import pandas as pd
-from io import StringIO
 
-uploaded_file = st.file_uploader("Choose a file")
-if uploaded_file is not None:
-    # To read file as bytes:
-    bytes_data = uploaded_file.getvalue()
-    st.write(bytes_data)
-
-    # To convert to a string based IO:
-    stringio = StringIO(uploaded_file.getvalue().decode("utf-8"))
-    st.write(stringio)
-
-    # To read file as string:
-    string_data = stringio.read()
-    st.write(string_data)
-
-    # Can be used wherever a "file-like" object is accepted:
-    dataframe = pd.read_csv(uploaded_file)
-    st.write(dataframe)
+   st.success("Password is valid")
