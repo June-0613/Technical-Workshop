29a4f28c94e9ad0d9491086f01e822bb3e1f49d1
June-0613 <juneelora6@gmail.com>
2025-03-21 12:34:38 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,34 +1,18 @@
 import streamlit as st
 
+
 st.title('Hello World')
 st.write('This is a simple Streamlit app.')
 
-import streamlit as st
 
-st.button("Reset", type="primary")
-if st.button("Say hello"):
-    st.write("Why hello there")
-else:
-    st.write("Goodbye")
+if st.button('Say hello'):
+   st.text('Hello, Streamlit!')
 
-import streamlit as st
-import pandas as pd
-from io import StringIO
 
-uploaded_file = st.file_uploader("Choose a file")
-if uploaded_file is not None:
-    # To read file as bytes:
-    bytes_data = uploaded_file.getvalue()
-    st.write(bytes_data)
+name = st.text_input('Please enter your name:')
+if name:
+   st.write(f'Hello, {name}!')
 
-    # To convert to a string based IO:
-    stringio = StringIO(uploaded_file.getvalue().decode("utf-8"))
-    st.write(stringio)
 
-    # To read file as string:
-    string_data = stringio.read()
-    st.write(string_data)
-
-    # Can be used wherever a "file-like" object is accepted:
-    dataframe = pd.read_csv(uploaded_file)
-    st.write(dataframe)
+if st.file_uploader('Please upload a file:', type=['txt', 'csv']):
+   st.write('Thanks for uploading a file!')
